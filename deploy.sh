6c098a8f1cb693568bfefeff7cb1b1c79cde583e
Mix-Liten <mixliten@gmail.com>
2022-11-07 00:20:38 +0800

    prepare deploy

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+timestamp=$(date +"%Y-%m-%d_%T")
+message='deploy_'
+message+=${timestamp}
+
+git init
+git checkout -B main
+git add -A
+git commit -m $message
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:mix-liten/react-hangman_game.git main:gh-pages
